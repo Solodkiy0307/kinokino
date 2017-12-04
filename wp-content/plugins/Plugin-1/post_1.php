39aa93722af4d8203d387700ac5974f0b4852996
Solodkiy0307 <solodkiy-1988@mail.ru>
2017-12-04 22:20:09 +0300

    создание плагина вывода данных таксономий и произвольных полей пока что без вывода

---
+++ wp-content/plugins/Plugin-1/post_1.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+Plugin Name: OutTax
+Plugin URI: http://страница_с_описанием_плагина_и_его_обновлений
+Description: выводит данные
+Version: 1.0
+Author: Solodkiy
+Author URI: http:solodkiy.dm@gmail.com
+*/
+
+/*  Copyright 2017  Solodkiy  (email: solodkiy.dm@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+//Активация плагина
+function install(){
+    global $wpdb;
+    $wpdb->query("CREATE TABLE `".$wpdb->prefix."popularposts` (
+        `ID` INT(10) UNSIGNED NULL AUTO_INCREMENT,
+        `count` INT DEFAULT '0',
+        `post_id` INT,
+        PRIMARY KEY (`ID`))");
+}
+register_activation_hook( __FILE__, 'install');
+
+//Деактивация плагина
+function deactivate(){
+    global $wpdb;
+    $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}popularposts");
+}
+register_deactivation_hook( __FILE__, 'deactivate');
+
+/* строку кода, чтобы зарегистрировать функцию, которая 
+будет вызываться в момент, когда WordPress формирует тег head страницы
+*/
+add_action ('wp_head', 'the_content');
